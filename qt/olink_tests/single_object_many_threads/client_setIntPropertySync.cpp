83bb093c13eff1da3f2a9708abd2978e83b25f88
dphan10 <dorota.phan@siili.com>
2024-04-03 09:24:28 +0200

    fix: fix test for set property

--- qt/olink_tests/single_object_many_threads/client_setIntPropertySync.cpp
+++ qt/olink_tests/single_object_many_threads/client_setIntPropertySync.cpp
@@ -19,8 +19,8 @@ public:
         sink = std::make_shared<TestSink>();
         sink->connect(sink.get(), &api::AbstractTestApi0::propIntChanged, [this](int propInt)
                                                                {
-                                                                   count++;
                                                                    m_latenciesStop[propInt-1]=std::chrono::high_resolution_clock::now();
+                                                                   count++;
                                                                });
     }
     bool allResponsesReceived (uint32_t sentRequestsNumber) const
@@ -33,7 +33,7 @@ public:
         m_latenciesStart[value] = std::chrono::high_resolution_clock::now();
         // Add one, to avoid setting property to 0 as first call, 0 is default property and it won't be set for same value.
         sink->setPropInt(value + 1);
-        while (count < value){}
+        while (count <= value){}
     }
 
     std::vector<std::chrono::time_point<std::chrono::high_resolution_clock>>& m_latenciesStart;
@@ -52,8 +52,8 @@ int main(int argc, char* argv[])
     QCoreApplication app(argc, argv);
 
     std::vector<uint16_t> timePerMessage;
-    auto sendThreadNumber = 100u;
-    auto messages_number = 1000u;
+    auto sendThreadNumber = 1;
+    auto messages_number = 1;
     if (argc > 1)
     {
         char* p;
