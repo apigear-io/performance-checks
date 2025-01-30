87bc91850a56d7e79ce47d4a696d9b2bd5d315e3
dorotaphanSiili <108282075+dorotaphanSiili@users.noreply.github.com>
2025-01-30 10:59:57 +0100

    update to 5.4

--- unreal_tests/Source/unreal_tests.Target.cs
+++ unreal_tests/Source/unreal_tests.Target.cs
@@ -5,11 +5,11 @@ using System.Collections.Generic;
 
 public class unreal_testsTarget : TargetRules
 {
-	public unreal_testsTarget( TargetInfo Target) : base(Target)
+	public unreal_testsTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
 		ExtraModuleNames.Add("unreal_tests");
 	}
 }
--- unreal_tests/Source/unreal_testsEditor.Target.cs
+++ unreal_tests/Source/unreal_testsEditor.Target.cs
@@ -8,8 +8,8 @@ public class unreal_testsEditorTarget : TargetRules
 	public unreal_testsEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
 		ExtraModuleNames.Add("unreal_tests");
 	}
 }
